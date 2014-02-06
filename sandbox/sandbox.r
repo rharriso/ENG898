812dd495a77fc8cdfacbf7dd66dee3241f438941
rharriso <rtharrison86@gmail.com>
2014-02-05 21:26:25 -0600

    Feb 5

--- sandbox/sandbox.r
+++ sandbox/sandbox.r
@@ -1,13 +1,2 @@
 # load from package
-require(tm)
-require(Snowball)
-require(SnowballC)
-require(XML)
-
-txt <- system.file("texts", "txt", package = "tm")
-ovid <- Corpus(DirSource(txt, encoding = "UTF-8"), readerControl = list(languages="lat"))
-
-reut21578 <- system.file("texts", "crude", package = "tm")
-reuters <- Corpus(DirSource(reut21578), 
-                  readerControl = list(reader = readReut21578XML))
-
+system("pwd")
