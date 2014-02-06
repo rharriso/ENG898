79dcd8d3f01fba54856ae6b66f82e240d6bf5254
rharriso <rtharrison86@gmail.com>
2014-02-05 22:42:25 -0600

    working on processing raw text files

--- sandbox/sandbox.r
+++ sandbox/sandbox.r
@@ -1,2 +1,9 @@
-# load from package
-system("pwd")
+lines <- readLines("texts/war_and_peace.txt")
+words <- scan("texts/war_and_peace.txt", character(0))
+wordsNoQuotes <- scan("texts/war_and_peace.txt", character(0), quote=NULL)
+sentences <- scan("texts/war_and_peace.txt", character(0), sep=".")
+
+require("tau")
+tokenize("here's a sentences. for you?")
+require("cwhmisc")
+substring.location("here's a sentences. for you?", "en")
