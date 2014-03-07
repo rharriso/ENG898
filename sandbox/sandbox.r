35f20284675dfdadf80d34830ff9649dad8f05d2
rharriso <rtharrison86@gmail.com>
2014-03-06 23:46:19 -0600

    entry about go web server

--- sandbox/sandbox.r
+++ sandbox/sandbox.r
@@ -24,11 +24,30 @@ novel.v <- text.v[start.v:end.v]
 # War and peace tagged in sentences
 #
 library(stringr)
-sentences <- scan("texts/war_and_peace.out.txt", character(0), sep=".")
+library(hash)
 
-# match and find in sentence
-s <- sentences[999]
-starr <- unlist(strsplit(s, " "))
-match_indices <- grep("/PERSON", starr)
-matches <- starr[match_indices]
-names <- gsub("/PERSON", "", matches) 
+# load in sentences
+sentences <- scan("texts/war_and_peace.out.txt", character(0), quote="", sep='.')
+# split sentenes into individual words
+starr <- strsplit(sentences, " ")
+
+characters <- new.env()
+
+for (i in 1:length(starr)) {
+  s <- unlist(starr[i])
+  match_indices <- grep("/PERSON", s)
+  matches <- s[match_indices]  
+  names <- gsub("/PERSON", "", matches) 
+  
+  if(length(names) > 0){
+    for (j in 1:length(names)){
+      n <- names[j]
+      
+      # don't visit empty strings
+      if(n != ""){
+        prev <-characters[[n]]
+        characters[[n]] <- c(prev, i) # add entry to vector
+      }   
+    }
+  }
+}
